3f67684dcc514f211832689c1fe5ad88ca3f5c7c
AbdalaMohB <120782158+AbdalaMohB@users.noreply.github.com>
2022-12-17 14:13:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Project2PerfectNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the second number");
+            int y = int.Parse(Console.ReadLine());
+            printperfect(x, y);
+            Console.ReadKey();
+        }
+        public static bool perfectcheck(int num)
+        {
+            int x = 1;
+
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    x += i;
+                }
+            }
+            if (num == x && num > 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void printperfect(int num1, int num2)
+        {
+            int o = num2 > num1 ? num2 : num1;//bigger
+            int p = num2 > num1 ? num1 : num2;//smaller
+            while (o >= p)
+            {
+                if (perfectcheck(p))
+                {
+                    Console.WriteLine(p);
+                }
+                ++p;
+            }
+        }
+    }
+}
